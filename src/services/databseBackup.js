59ffb7dfaa6ff5550267e94a85912c916d85325c
Muskan Singh <singhmuskan2603@gmail.com>
2023-01-01 00:01:22 +0530

    folder-structures

--- src/services/databseBackup.js
+++ src/services/databseBackup.js
@@ -2,13 +2,13 @@ const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-const backupDir = '/mnt/backup_drive';  // Replace with your desired backup directory
+const backupDir = '/mnt/backup_drive';  
 
-// Create timestamped directory for each backup
+
 const timestamp = new Date().toISOString().replace(/[-:.]/g, '');
 const backupDirTimestamped = path.join(backupDir, `backup_${timestamp}`);
 
-// Ensure backup directory exists
+
 try {
   fs.mkdirSync(backupDirTimestamped, { recursive: true });
   console.log(`Created backup directory: ${backupDirTimestamped}`);
@@ -17,7 +17,7 @@ try {
   process.exit(1);
 }
 
-// Function to execute shell commands for database backups
+
 function executeBackupCommand(command, dbName, dbType) {
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
@@ -32,22 +32,20 @@ function executeBackupCommand(command, dbName, dbType) {
   });
 }
 
-// MySQL/MariaDB/Percona backup
+
 const mysqlDumpCommand = `mysqldump -u <username> -p<password> --all-databases > ${path.join(backupDirTimestamped, 'mysql_backup.sql')}`;
 executeBackupCommand(mysqlDumpCommand, 'MySQL/MariaDB/Percona', 'MySQL')
   .catch(err => console.error(err));
 
-// PostgreSQL backup
 const pgDumpCommand = `pg_dumpall -U <username> -f ${path.join(backupDirTimestamped, 'postgres_backup.sql')}`;
 executeBackupCommand(pgDumpCommand, 'PostgreSQL', 'PostgreSQL')
   .catch(err => console.error(err));
 
-// MongoDB backup
 const mongoDumpCommand = `mongodump --username <username> --password <password> --out ${path.join(backupDirTimestamped, 'mongodb_backup')}`;
 executeBackupCommand(mongoDumpCommand, 'MongoDB', 'MongoDB')
   .catch(err => console.error(err));
 
-// Redis backup (snapshot)
+
 const redisSaveCommand = `redis-cli SAVE`;
 executeBackupCommand(redisSaveCommand, 'Redis', 'Redis')
   .catch(err => console.error(err));
